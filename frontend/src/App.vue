bfa328bdb52b31b8c6c35df948c697b975c704e5
Tristan Adams <Tristan@tristanadams.me>
2022-01-06 11:25:12 -0700

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
 
   <FeaturedUser/>
   <div class="alert alert-success alert-dismissible fade show" role="alert">
-    With Bootstrap!!!
+    With Bootstrap!!!!!!
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
